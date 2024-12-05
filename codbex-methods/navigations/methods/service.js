4a2c97e543e1bdc0ff47a50181e6da38e4a79a20
TIVMOF <78173711+TIVMOF@users.noreply.github.com>
2024-12-05 12:30:50 +0200

    New Dashboard Structure (#7)

--- codbex-methods/navigations/methods/service.js
+++ codbex-methods/navigations/methods/service.js
@@ -1,10 +1,8 @@
 const navigationData = {
     id: 'methods-navigation',
     label: "Payment Methods",
-    view: "methods",
-    group: "configurations",
-    orderNumber: 1000,
-    lazyLoad: true,
+    group: "metadata",
+    order: 2400,
     link: "/services/web/codbex-methods/gen/codbex-methods/ui/Methods/index.html?embedded"
 };
 
