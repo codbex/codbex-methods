f7109fde6d678b3e9ae583cecb48d94b5471800d
Nikol Georgieva <nicole.georgieva18@abv.bg>
2025-02-20 18:03:57 +0200

    Split the entities into two navigations - for Sent and Payment methods (#10)

---
+++ codbex-methods/navigations/sent-method/service.js
@@ -0,0 +1,17 @@
+const navigationData = {
+    id: 'methods-navigation',
+    label: "Sent Methods",
+    group: "reference data",
+    order: 2600,
+    link: "/services/web/codbex-methods/gen/codbex-methods/ui/SentMethod/index.html?embedded"
+};
+
+function getNavigation() {
+    return navigationData;
+}
+
+if (typeof exports !== 'undefined') {
+    exports.getNavigation = getNavigation;
+}
+
+export { getNavigation }
