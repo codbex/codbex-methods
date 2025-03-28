70ff362491e5c6d1076d30f76522cba83d87786e
Nikol Georgieva <nicole.georgieva18@abv.bg>
2025-03-28 12:56:10 +0200

    change navigation order (#12)

--- codbex-methods/navigations/payment-method/service.js
+++ codbex-methods/navigations/payment-method/service.js
@@ -2,7 +2,7 @@ const navigationData = {
     id: 'payment-method-navigation',
     label: "Payment Methods",
     group: "reference data",
-    order: 2400,
+    order: 2500,
     link: "/services/web/codbex-methods/gen/codbex-methods/ui/PaymentMethod/index.html?embedded"
 };
 
