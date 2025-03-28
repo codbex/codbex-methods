8c9a4b71a777ed9b5b06b3e2a579576d8de67850
NicoleNG18 <nicole.georgieva18@abv.bg>
2025-03-28 10:39:02 +0200

    change navigation order

--- codbex-methods/navigations/sent-method/service.js
+++ codbex-methods/navigations/sent-method/service.js
@@ -2,7 +2,7 @@ const navigationData = {
     id: 'methods-navigation',
     label: "Sent Methods",
     group: "reference data",
-    order: 2600,
+    order: 2700,
     link: "/services/web/codbex-methods/gen/codbex-methods/ui/SentMethod/index.html?embedded"
 };
 
