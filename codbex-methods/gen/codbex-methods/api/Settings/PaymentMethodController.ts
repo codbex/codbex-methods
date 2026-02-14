1a9291e672e5e45d74d8168b50c42c06e37089fa
Nikol Georgieva <nicole.georgieva18@abv.bg>
2026-02-14 11:10:26 +0200

    regenerate module using Application Full Stack - v2 template (#17)

---
+++ codbex-methods/gen/codbex-methods/api/Settings/PaymentMethodController.ts
@@ -0,0 +1,175 @@
+import { Controller, Get, Post, Put, Delete, Documentation, request, response } from '@aerokit/sdk/http'
+import { HttpUtils } from "@aerokit/sdk/http/utils";
+import { ValidationError } from '@aerokit/sdk/http/errors'
+import { ForbiddenError } from '@aerokit/sdk/http/errors'
+import { user } from '@aerokit/sdk/security'
+import { Options } from '@aerokit/sdk/db'
+import { Extensions } from "@aerokit/sdk/extensions"
+import { Injected, Inject } from '@aerokit/sdk/component'
+import { PaymentMethodRepository } from '../../data/Settings/PaymentMethodRepository'
+import { PaymentMethodEntity } from '../../data/Settings/PaymentMethodEntity'
+
+const validationModules = await Extensions.loadExtensionModules('codbex-methods-Settings-PaymentMethod', ['validate']);
+
+@Controller
+@Documentation('codbex-methods - PaymentMethod Controller')
+@Injected()
+class PaymentMethodController {
+
+    @Inject('PaymentMethodRepository')
+    private readonly repository!: PaymentMethodRepository;
+
+    @Get('/')
+    @Documentation('Get All PaymentMethod')
+    public getAll(_: any, ctx: any): PaymentMethodEntity[] {
+        try {
+            this.checkPermissions('read');
+            const options: Options = {
+                limit: ctx.queryParameters["$limit"] ? parseInt(ctx.queryParameters["$limit"]) : 20,
+                offset: ctx.queryParameters["$offset"] ? parseInt(ctx.queryParameters["$offset"]) : 0,
+                language: request.getLocale().slice(0, 2)
+            };
+
+            return this.repository.findAll(options);
+        } catch (error: any) {
+            this.handleError(error);
+        }
+        return undefined as any;
+    }
+
+    @Post('/')
+    @Documentation('Create PaymentMethod')
+    public create(entity: PaymentMethodEntity): PaymentMethodEntity {
+        try {
+            this.checkPermissions('write');
+            this.validateEntity(entity);
+            entity.Id = this.repository.create(entity) as any;
+            response.setHeader('Content-Location', '/services/ts/codbex-methods/gen/codbex-methods/api/Settings/PaymentMethodService.ts/' + entity.Id);
+            response.setStatus(response.CREATED);
+            return entity;
+        } catch (error: any) {
+            this.handleError(error);
+        }
+        return undefined as any;
+    }
+
+    @Get('/count')
+    @Documentation('Count PaymentMethod')
+    public count(): { count: number } {
+        try {
+            this.checkPermissions('read');
+            return { count: this.repository.count() };
+        } catch (error: any) {
+            this.handleError(error);
+        }
+        return undefined as any;
+    }
+
+    @Post('/count')
+    @Documentation('Count PaymentMethod with filter')
+    public countWithFilter(filter: any): { count: number } {
+        try {
+            this.checkPermissions('read');
+            return { count: this.repository.count(filter) };
+        } catch (error: any) {
+            this.handleError(error);
+        }
+        return undefined as any;
+    }
+
+    @Post('/search')
+    @Documentation('Search PaymentMethod')
+    public search(filter: any): PaymentMethodEntity[] {
+        try {
+            this.checkPermissions('read');
+            return this.repository.findAll(filter);
+        } catch (error: any) {
+            this.handleError(error);
+        }
+        return undefined as any;
+    }
+
+    @Get('/:id')
+    @Documentation('Get PaymentMethod by id')
+    public getById(_: any, ctx: any): PaymentMethodEntity {
+        try {
+            this.checkPermissions('read');
+            const id = parseInt(ctx.pathParameters.id);
+            const options: Options = {
+                language: request.getLocale().slice(0, 2)
+            };
+            const entity = this.repository.findById(id, options);
+            if (entity) {
+                return entity;
+            } else {
+                HttpUtils.sendResponseNotFound('PaymentMethod not found');
+            }
+        } catch (error: any) {
+            this.handleError(error);
+        }
+        return undefined as any;
+    }
+
+    @Put('/:id')
+    @Documentation('Update PaymentMethod by id')
+    public update(entity: PaymentMethodEntity, ctx: any): PaymentMethodEntity {
+        try {
+            this.checkPermissions('write');
+            const id = parseInt(ctx.pathParameters.id);
+            entity.Id = id;
+            this.validateEntity(entity);
+            this.repository.update(entity);
+            return entity;
+        } catch (error: any) {
+            this.handleError(error);
+        }
+        return undefined as any;
+    }
+
+    @Delete('/:id')
+    @Documentation('Delete PaymentMethod by id')
+    public deleteById(_: any, ctx: any): void {
+        try {
+            this.checkPermissions('write');
+            const id = parseInt(ctx.pathParameters.id);
+            const entity = this.repository.findById(id);
+            if (entity) {
+                this.repository.deleteById(id);
+                HttpUtils.sendResponseNoContent();
+            } else {
+                HttpUtils.sendResponseNotFound('PaymentMethod not found');
+            }
+        } catch (error: any) {
+            this.handleError(error);
+        }
+    }
+
+    private handleError(error: any) {
+        if (error.name === 'ForbiddenError') {
+            HttpUtils.sendForbiddenRequest(error.message);
+        } else if (error.name === 'ValidationError') {
+            HttpUtils.sendResponseBadRequest(error.message);
+        } else {
+            HttpUtils.sendInternalServerError(error.message);
+        }
+    }
+
+    private checkPermissions(operationType: string) {
+        if (operationType === 'read' && !(user.isInRole('codbex-methods.PaymentMethod.PaymentMethodReadOnly') || user.isInRole('codbex-methods.PaymentMethod.PaymentMethodFullAccess'))) {
+            throw new ForbiddenError();
+        }
+        if (operationType === 'write' && !user.isInRole('codbex-methods.PaymentMethod.PaymentMethodFullAccess')) {
+            throw new ForbiddenError();
+        }
+    }
+
+    private validateEntity(entity: any): void {
+        if (entity.Name?.length > 20) {
+            throw new ValidationError(`The 'Name' exceeds the maximum length of [20] characters`);
+        }
+        for (const next of validationModules) {
+            next.validate(entity);
+        }
+    }
+
+}
