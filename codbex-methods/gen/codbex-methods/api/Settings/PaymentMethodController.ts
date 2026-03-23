c7de0c1a5a3f2d64eac0ac0e4bb9885ae314f986
ThuF <jordan.p.pavlov@gmail.com>
2026-03-23 15:07:13 +0000

    Migrate from project.json to package.json

--- codbex-methods/gen/codbex-methods/api/Settings/PaymentMethodController.ts
+++ codbex-methods/gen/codbex-methods/api/Settings/PaymentMethodController.ts
@@ -44,7 +44,7 @@ class PaymentMethodController {
             this.checkPermissions('write');
             this.validateEntity(entity);
             entity.Id = this.repository.create(entity) as any;
-            response.setHeader('Content-Location', '/services/ts/codbex-methods/gen/codbex-methods/api/Settings/PaymentMethodService.ts/' + entity.Id);
+            response.setHeader('Content-Location', '/services/ts/codbex-methods/gen/codbex-methods/api/Settings/PaymentMethodController.ts/' + entity.Id);
             response.setStatus(response.CREATED);
             return entity;
         } catch (error: any) {
--- codbex-methods/gen/codbex-methods/api/Settings/SentMethodController.ts
+++ codbex-methods/gen/codbex-methods/api/Settings/SentMethodController.ts
@@ -44,7 +44,7 @@ class SentMethodController {
             this.checkPermissions('write');
             this.validateEntity(entity);
             entity.Id = this.repository.create(entity) as any;
-            response.setHeader('Content-Location', '/services/ts/codbex-methods/gen/codbex-methods/api/Settings/SentMethodService.ts/' + entity.Id);
+            response.setHeader('Content-Location', '/services/ts/codbex-methods/gen/codbex-methods/api/Settings/SentMethodController.ts/' + entity.Id);
             response.setStatus(response.CREATED);
             return entity;
         } catch (error: any) {
